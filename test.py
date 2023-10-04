48b02780c7f9266391c37c37c4cd2cc38d58763e
William Huang <whuang7337@gmail.com>
2023-10-04 16:45:27 -0700

    sample .py file

---
+++ test.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    x = input("Which course are you taking?")
+    if x == "ECE 180":
+        print(x + " is the best class ever")
+    else:
+        print("Bruins are the best!")
